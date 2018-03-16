095ccbc71df09fe1f75104381004fa104f727341
Festus kiambi <kiamfestoh@gmail.com>
2018-03-16 17:30:25 +0300

    authenticate user tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
     factory :user do
         name {Faker::Name.name}
-        email {Faker::Email.email}
+        email 'foobar@email.com'
         password_digest 'foobar'
     end    
 end    
